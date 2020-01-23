b265d3f3ed06347d3b6aee8c131ee1c12d880494
sandtell <sandtellcustomercare@gmail.com>
2020-01-23 18:33:15 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import xml2js from 'xml2js';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public xmlToJson;
+  public isShowHide:boolean = false;
+  constructor(private barcodeScanner: BarcodeScanner) { }
+
+
+  scanQR() {
+    
+    // Optionally request the permission early
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.convertToJson(barcodeData.text);
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+        alert(JSON.stringify(err));
+         console.log('Error', err);
+     });
+  }
+
+  convertToJson(data: string): Object {
+    let res;
+    // setting the explicitArray option prevents a array structure
+    // where every node/element is always wrapped inside an array
+    // set it to true, and see for yourself what changes
+    xml2js.parseString(data, { explicitArray: false }, (error, result) => {      
+      if (error) {
+        throw new Error(error);
+      } else {
+        this.isShowHide = true;
+        res = result;
+        this.xmlToJson = res;
+        // alert(res.PrintLetterBarcodeData.$.uid);
+      }
+    });
+    return res;
+  }
+
+  
+  public convertToXml(rootObject:Object) {
+    return new xml2js.Builder().buildObject(rootObject);
+  }
+
 
 }
